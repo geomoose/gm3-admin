a3dfdd8d6f62a539beed098632bca18d87344be5
Jim Klassen <klassen.js@gmail.com>
2017-06-26 11:42:40 -0500

    Update copyright date

--- build-examples-packages.sh
+++ build-examples-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # The MIT License (MIT)
-# Copyright (c) 2016 GeoMoose
+# Copyright (c) 2017 GeoMoose
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
